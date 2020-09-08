25997b47fa1e8991a2bf550d7812f440a41e13e8
guichen01 <guichen01@baidu.com>
2020-09-08 19:34:01 +0800

    init

---
+++ src/main/java/me/foreti/leetcode/array/_1_two_sum/TwoSum.java
@@ -0,0 +1,15 @@
+package me.foreti.leetcode.array._1_two_sum;
+
+/**
+ * description
+ *
+ * @author guichen01
+ * create date: 2020/9/8
+ */
+public class TwoSum {
+
+    public int[] solution() {
+
+    }
+
+}
